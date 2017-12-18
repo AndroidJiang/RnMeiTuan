c9513ef60cdc885bec04d2a82fe7106a4d0a036f
liuyan <15751320195@163.com>
2017-12-18 14:53:41 +0800

    修改

--- App.js
+++ App.js
@@ -13,8 +13,6 @@ import {
 } from 'react-native';
 
 const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
   android: 'Double tap R on your keyboard to reload,\n' +
     'Shake or press menu button for dev menu',
 });
