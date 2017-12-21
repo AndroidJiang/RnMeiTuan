c0388ceb4c43aec15206b6fc5f42fa810eab6e00
AJiang <yanjiang@91guoxin.com>
2017-12-21 16:39:29 +0800

    登录ui

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 import { AppRegistry } from 'react-native';
-import App from './App';
+import Login from "./Login";
 
-AppRegistry.registerComponent('RnDemo', () => App);
+AppRegistry.registerComponent('RnDemo', () => Login);
